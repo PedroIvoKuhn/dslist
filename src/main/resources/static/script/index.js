b65188e0d4b8fbea1076c6db6e5a6c7c3335aa87
Pedro Ivo Kuhn <kuhnpedroivo@gmail.com>
2023-12-27 00:22:44 -0300

    Aplicando css

--- src/main/resources/static/script/index.js
+++ src/main/resources/static/script/index.js
@@ -1,23 +1,3 @@
-/*
-window.onload = function(){
-    fetch('http://localhost:8080/games')
-            .then(response => response.json())
-            .then(data => {
-                var lista = document.getElementById('minhaLista');
-                data.forEach(item => {
-                    var li = document.createElement("li");
-                    var a = document.createElement("a");
-                    a.textContent = item.title
-                    a.href = `/pages/GameDetails.html?id=${item.id}`
-                    document.li.appendChild(a);
-                    li.classList.add("elemento")
-                    document.lista.appendChild(li);
-                    lista.style.listStyleType = 'none';
-                });
-            })
-            .catch(error => console.error('Erro:', error));
-}
-*/
 window.onload = function(){
     fetch('http://localhost:8080/games')
         .then(response => response.json())
@@ -25,15 +5,26 @@ window.onload = function(){
             const lista = document.getElementById('minhaLista');
             data.forEach(item => {
                 const li = document.createElement("li");
-                const a = document.createElement("a");
-                a.textContent = item.title;
-                a.href = `/pages/GameDetails.html?id=${item.id}`;
-                a.style.textDecoration = 'none';
-                li.appendChild(a); //'li', não 'document.li'
-                li.classList.add("elemento");
-                lista.appendChild(li); //'lista', não 'document.lista'
+                const div = document.createElement("div");
+                    const img = document.createElement("img");
+                    img.src = item.imgUrl;
+                    img.style.width = "100px";
+                    
+                    div.appendChild(img);
+                    const a = document.createElement("a");
+                    a.textContent = item.title;
+                    a.href = `/pages/GameDetails.html?id=${item.id}`;
+                    a.style.textDecoration = 'none';
+                    div.style.display = 'flex';
+                    div.style.alignItems = 'center';
+                    div.style.justifyContent = 'space-between';
+                    div.style.margin = '10px';
+
+                    div.appendChild(a);
+                li.appendChild(div);
+                lista.appendChild(li);
             });
-            lista.style.listStyleType = 'none'; // Movido para fora do loop
+            lista.style.listStyleType = 'none';
         })
         .catch(error => console.error('Erro:', error));
 }
